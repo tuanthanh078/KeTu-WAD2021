71aab537c6f14cf0ae987bfb9644a208faa4f628
tuanthanhtran <tuanthanh078@gmail.com>
2021-06-19 21:54:26 +0200

    implement endpoint /users

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -26,20 +26,17 @@ var mitte;
 // var isLoggedIn = false;
 // var isAdmina = false;
 
-function User(username, password/*, contacts*/, isAdmin){
+function User(username, isAdmin){
 	this.username = username;
-	this.password = password;
-	//this.contacts = contacts;
 	this.isAdmin = isAdmin;
-	
+
 	var option = document.createElement("option");
 	option.appendChild(document.createTextNode(username));
 	addForm.owner.appendChild(option);
 }
-	
-var admina = new User("admina", "admina"/*, [contact1, contact2]*/, true);
-var normalo = new User("normalo", "normalo"/*, [contact3, contact4]*/, false);
 
+var admina = new User("admina", true);
+var normalo = new User("normalo", false);
 //Alle hardcoded nutzer (admina, normalo)
 var users = [admina, normalo];
 
@@ -91,16 +88,16 @@ function createForm(requiredFields, submitButtons){
 	for(let button of submitButtons){
 		button.disabled = true;
 	}
-	
+
 	for(let i = 0; i<requiredFields.length; i++){
 		requiredFields[i].valid = false;
 	}
-	
+
 	for(let i = 0; i<requiredFields.length; i++){
 		requiredFields[i].addEventListener("keyup", (e) => {
-			
+
 			requiredFields[i].valid = requiredFields[i].checkValidity();
-			
+
 			for(let j = 0; j<requiredFields.length; j++){
 				if(requiredFields[j].valid == false){
 					for(let button of submitButtons){
@@ -131,45 +128,51 @@ loginButton.addEventListener("click", (e) => {
 	e.preventDefault();
 	const username = loginForm.username.value;
 	const password = loginForm.password.value;
-	
-	let loginCorrect = false;
-  
-	for(let i = 0; i< users.length;i++){
-		if(username === users[i].username &&
-			password === users[i].password){
-			
-			//Setze Login war korrekt
-			loginCorrect = true;
-			
-			//Setze aktuellen User
-			currUser = users[i];	
-			
-			//Wechsel zu mainScreen
-			loginScreen.style.display = "none";
-			mainScreen.style.display = "block";		
 
-			//Zeige Header mit Begruessung und Logout Button
-			header.style.display = "block";
-			headerInfo.innerHTML = "Hello "+currUser.username+"!";
-			
-			//Lade Karten Marker und Kontaktliste
-			reloadAddressListUser();
-			loadMarkers();
-			
-			//Entferne Admin Felder fuer Normalo
-			if(!currUser.isAdmin){
-				ownerSelect.remove();
-			}
-			
-			break;
+	let loginCorrect = false;
+
+	let httpRequest = new XMLHttpRequest();
+	let url = "http://localhost:3000/users";
+
+	httpRequest.open("POST", url, true);
+	httpRequest.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+	httpRequest.onerror = function() {// diese Funktion wird ausgefuehrt, wenn ein Fehler auftritt
+			console.log("Connecting to server with " + url + " failed!\n");
+	};
+
+	httpRequest.onreadystatechange = function() {//Call a function when the state changes.
+		if(httpRequest.readyState == 4 && httpRequest.status == 200) {
+				//Setze Login war korrekt
+				loginCorrect = true;
+
+				// let pojo = JSON.parse(httpRequest.responseText);
+				//Setze aktuellen User
+				currUser = JSON.parse(httpRequest.responseText);
+				if (currUser.isAdmin)
+					currUser = admina;
+				else
+					currUser = normalo;
+
+				//Wechsel zu mainScreen
+				loginScreen.style.display = "none";
+				mainScreen.style.display = "block";
+				//Zeige Header mit Begruessung und Logout Button
+				header.style.display = "block";
+				headerInfo.innerHTML = "Hello "+currUser.username+"!";
+				//Lade Karten Marker und Kontaktliste
+				reloadAddressListUser();
+				loadMarkers();
+				//Entferne Admin Felder fuer Normalo
+				if(!currUser.isAdmin){
+					ownerSelect.remove();
+				}
 		}
-	}
-	
-	if(loginCorrect){
-		alert("You have successfully logged in");
-	}else{
-		alert("Wrong username or password!");
-	}
+	};
+
+	httpRequest.send(`username=${username}&password=${password}`);
+
+	setTimeout(function(){(loginCorrect) ? alert("You have successfully logged in") : alert("Wrong username or password!");}, 100);
 });
 
 //Logout fuer den aktuellen nutzer, wechsel zu login screen
@@ -185,12 +188,12 @@ logoutButton.addEventListener("click", (e) => {
 	if(!currUser.isAdmin){
 		addForm.insertBefore(ownerSelect, document.getElementById("private-row"));
 	}
-	
+
 	for(let contact of contacts){
 		setMarker(contact, false);
 	}
-	
-	
+
+
 	setValue(loginForm.password, "");
 	loginButton.disabled = true;
 });
@@ -217,7 +220,7 @@ function reloadAddressList(){
 //Laedt die address Liste fuer den aktuellen Nutzer
 function reloadAddressListUser() {
 	reloadAddressList();
-	
+
 	if(userOnlyAddresses === false){
 		for(let contact of contacts){
 			if(contact.owner !== currUser && (currUser.isAdmin === true || contact.isPrivate === false)){
@@ -225,7 +228,7 @@ function reloadAddressListUser() {
 			}
 		}
 	}
-	
+
 	userOnlyAddresses = true;
 }
 
@@ -233,7 +236,7 @@ function reloadAddressListUser() {
 //Wenn Admin inklusive aller anderen Adressen, ansonsten nur publik Adressen
 function reloadAddressListAll(){
 	reloadAddressList();
-	
+
 	for(let contact of contacts){
 		if(contact.owner !== currUser && (currUser.isAdmin === true || contact.isPrivate === false)){
 			addToAddressList(contact);
@@ -252,9 +255,9 @@ function addToAddressList(contact){
 	let addressLi = document.createElement("li");
 	addressLi.innerHTML = fullname(contact);
 	addressLi.className = "address";
-			
+
 	addressLi.addEventListener("click", (e) => {
-			
+
 		if(currUser.isAdmin == false && contact.owner.username !== currUser.username){
 			addForm.appendChild(backButton);
 		}else{
@@ -262,7 +265,7 @@ function addToAddressList(contact){
 			updateButton.disabled = false;
 			addForm.appendChild(updateButtons);
 		}
-			
+
 		setValue(addForm.firstname, contact.firstname);
 		setValue(addForm.lastname, contact.lastname);
 		setValue(addForm.street, contact.street);
@@ -272,7 +275,7 @@ function addToAddressList(contact){
 		setValue(addForm.state, contact.state);
 		setValue(addForm.country, contact.country);
 		addForm.private.checked = contact.isPrivate;
-		
+
 		//Setzte den korrekten owner des ausgewaehlten Kontakts
 		if(currUser.isAdmin){
 			for(let i = 0; i<users.length; i++){
@@ -290,7 +293,7 @@ function addToAddressList(contact){
 				}
 			}*/
 		}
-		
+
 		mainScreen.style.display = "none";
 		header.style.display = "none";
 		addScreen.style.display = "block";
@@ -342,20 +345,20 @@ function loadMarkers() {
 //Gibt entsprechenden alert wenn kein Marker erstellt werden konnte
 //Wird visible auf true gesetzt oder nicht spezifiziert, wird der Marker sobald er erstellt wurde auf der Karte angezeigt
 function setMarker(contact, visible){
-	
+
 	let selMap = null;
 	if(visible === true || visible === undefined){
 		selMap = map;
 	}
-	
+
 	if(contact.markerGenerated === true){
 		try{
 			contact.marker.setMap(selMap);
 		}catch(e){
-			
+
 		}
 	}else{
-	
+
 		var xhr = new XMLHttpRequest();
 		var url = "https://maps.googleapis.com/maps/api/geocode/json?";
 		url = url + "address=" + address(contact);
@@ -393,7 +396,7 @@ function setMarker(contact, visible){
 
 //Wechselt in den add Contact screen
 addNewButton.addEventListener("click", (e) => {
-	
+
 	addButtons.disabled = true;
 
 	setValue(addForm.firstname, "");
@@ -405,25 +408,25 @@ addNewButton.addEventListener("click", (e) => {
 	setValue(addForm.state, "");
 	setValue(addForm.country, "");
 	addForm.private.checked = true;
-	
+
 	mainScreen.style.display = "none";
 	header.style.display = "none";
-	
+
 	//Prepare and show addScreen
 	addScreen.appendChild(addButtons);
 	addButtons.form = "add-form";
 	addScreen.style.display = "block";
-	
+
 });
 
 //Fuegt den durch das addForm spezifiezierten Kontakt hinzu
 addButtons.addEventListener("click", (e) => {
-	
+
 	let owner = currUser;
 	if(currUser.isAdmin){
 		owner = users[addForm.owner.selectedIndex];
 	}
-	
+
 	let contactInput = new Contact(
 			addForm.firstname.value,
 			addForm.lastname.value,
@@ -433,21 +436,21 @@ addButtons.addEventListener("click", (e) => {
 			addForm.city.value,
 			addForm.state.value,
 			addForm.country.value,
-			addForm.private.value,	
+			addForm.private.value,
 			owner
-		);	
-	
+		);
+
 	if(userOnlyAddresses && owner!==currUser){
 		setMarker(contactInput, false);
 	}else{
 		setMarker(contactInput);
 		addToAddressList(contactInput);
 	}
-	
+
 	contacts.push(contactInput);
-	
-	addButtons.remove();	
-	
+
+	addButtons.remove();
+
 	addScreen.style.display = "none";
 	mainScreen.style.display = "block";
 	header.style.display = "block";
@@ -456,21 +459,21 @@ addButtons.addEventListener("click", (e) => {
 //Aktuellisiert den ausgewaehlten Kontakt durch die im addForm
 //gewaehlten spezifikationen
 updateButton.addEventListener("click", (e) => {
-	
+
 	var addressChanged = false;
 	if(selectedContact.street != addForm.street.value
 		|| selectedContact.streetnr != addForm.streetnr.value
 		|| selectedContact.zip != addForm.zip.value
 		|| selectedContact.city != addForm.city.value){
-		
+
 		addressChanged = true;
 	}
-	
+
 	selectedContact.firstname = addForm.firstname.value;
 	selectedContact.lastname = addForm.lastname.value;
-	
+
 	selectedContact.marker.setTitle(fullname(selectedContact));
-	
+
 	selectedContact.street = addForm.street.value;
 	selectedContact.streetnr = addForm.streetnr.value;
 	selectedContact.zip = addForm.zip.value;
@@ -486,21 +489,21 @@ updateButton.addEventListener("click", (e) => {
 	}else{
 		selectedContact.owner = currUser;
 	}
-	
+
 	if(addressChanged === true){
 		selectedContact.marker.setMap(null);
 		selectedContact.markerGenerated = false;
 		setMarker(selectedContact);
 	}
-	
+
 	if(userOnlyAddresses === true){
 		reloadAddressListUser();
 	}else{
 		reloadAddressListAll();
 	}
-	
+
 	updateButtons.remove();
-	
+
 	addScreen.style.display = "none";
 	mainScreen.style.display = "block";
 	header.style.display = "block";
@@ -509,37 +512,37 @@ updateButton.addEventListener("click", (e) => {
 //Loescht den ausgewaehlten Kontakt
 deleteButton.addEventListener("click", (e) => {
 	selectedContact.marker.setMap(null);
-	
+
 	for(let i = 0; i<contacts.length; i++){
 		if(contacts[i] === selectedContact){
 			contacts.splice(i,1);
 		}
 	}
-	
+
 	/*let owner = getOwner(selectedContact);
 	for(let i = 0; i<owner.contacts.length; i++){
 		if(owner.contacts[i] == selectedContact){
 			owner.contacts.splice(i,1);
 		}
 	}*/
-	
+
 	if(userOnlyAddresses === true){
 		reloadAddressListUser();
 	}else{
 		reloadAddressListAll();
 	}
-	
+
 	updateButtons.remove();
-	
+
 	addScreen.style.display = "none";
 	mainScreen.style.display = "block";
 	header.style.display = "block";
 });
 
-//Kehrt vom Update- zum MainScreen zurueck 
+//Kehrt vom Update- zum MainScreen zurueck
 backButton.addEventListener("click", (e) => {
 	backButton.remove();
-	
+
 	addScreen.style.display = "none";
 	mainScreen.style.display = "block";
 	header.style.display = "block";
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+function User(username, password, isAdmin){
+	this.username = username;
+	this.password = password;
+	this.isAdmin = isAdmin;
+}
+
+var admina = new User("admina", "admina", true);
+var normalo = new User("normalo", "normalo", false);
+var users = [admina, normalo];
+
+router.post('/', function(req, res, next) {
+  let username = req.body.username;
+  let password = req.body.password;
+  console.log("[ROUTER users.js] post");
+  console.log(`Username: ${username} Password: ${password}`);
+
+  let loginCorrect = false;
+
+  for (let i = 0; i < users.length; i++) {
+    if(username === users[i].username &&
+      password === users[i].password){
+
+      //Setze Login war korrekt
+      loginCorrect = true;
+
+      res.status(200).json({"username": users[i].username, "isAdmin": users[i].isAdmin});
+
+      break;
+    }
+  }
+
+  if (!loginCorrect)
+    res.status(401);
+  res.end();
 });
 
 module.exports = router;
